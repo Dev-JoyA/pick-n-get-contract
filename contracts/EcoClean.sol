5846e0e66d4bb204cb263ecde0af213302346862
Joy A <joy.gold13@gmail.com>
2025-08-25 15:43:48 +0100

    user contract

--- contracts/EcoClean.sol
+++ contracts/EcoClean.sol
@@ -2,5 +2,24 @@
 pragma solidity ^0.8.28;
 
 contract EcoClean {
+    // user will recycle item
     //admin will pay user
+    // swap stable coin for fiat - FE
+
+    mapping (address => bool) isPaid;
+
+    // shop within the site using hbar 
+
+    //  function recievedPayment(address _user) internal view{
+    //     for(uint256 i = 0; i < users.length; i++){
+    //         if(users[i] != _user){
+    //             revert NotFound();
+    //         }
+    //     }
+
+    //     uint256 _id = userId[_user]; 
+    //     if(userAccountId[_id].userAddress != _user){
+    //         revert UserNotRegistered();
+    //     }  
+    // }
 }
