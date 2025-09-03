f55965143dc8ebb36f4749ff66cac4cd4d3e002d
Joy A <joy.gold13@gmail.com>
2025-09-03 21:02:12 +0100

    interface added

---
+++ contracts/interface/IEcoClean.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+interface IEcoClean
+ {
+   event ItemRecycled(address indexed user, uint256 itemId, string itemType, uint256 weight);
+
+    function registerUser() external ;
+
+    function registerAdmin(address _admin) external;
+
+    function registerProducer(string memory  _name, string memory _country, uint256 _number) external ;
+    
+    function recycleItem(string memory _type, uint256 _weight) external ;
+
+    function payUser(uint256 _id, uint256 _rid) external payable ;
+
+    function deleteUserAccount(address _user) external ;
+
+    function deleteAdmin(address _admin) external;
+
+    function deleteAdminById(uint256 _adminId) external;
+
+    function setRate(uint256 _rate) external ;
+
+    function addProduct(uint256 _id, string memory _name, uint256 _quantity, bytes memory _data, uint256 _amount) external;
+
+    function shopProduct(uint256 _pid, uint256 _quantity) external payable;
+
+    function fundContract() external payable ;
+
+    function contractBalance() external view returns (uint256);
+}
